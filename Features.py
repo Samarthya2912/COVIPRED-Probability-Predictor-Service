a46b5bc2b36b3dabce299354fd7762dec3dbe64c
Samarthya Singh <samarthya55@gmail.com>
2022-10-19 00:00:09 +0530

    Minor update

--- Features.py
+++ Features.py
@@ -4,5 +4,3 @@ def getFeatures():
     features = ['cough', 'fever', 'sore_throat', 'shortness_of_breath',
             'head_ache', 'age_60_and_above', 'gender', 'test_indication']
     return features
-
-sys.modules['getFeatures'] = getFeatures
